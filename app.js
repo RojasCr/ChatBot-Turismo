d06b6cb98696ff78454a0d09347ba848ec8d54c7
Cristian Rojas <cristianrojas781@gmail.com>
2025-06-03 20:48:33 -0300

    3-6-2025 20:48

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const historialFlow = addKeyword("Reservas")
 })
 
 const barilocheFlow = addKeyword(EVENTS.ACTION)
-.addAnswer(barilocheMsg, {delay: 1000, media: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Labrador_Retriever_%281210559%29.jpg/960px-Labrador_Retriever_%281210559%29.jpg"})
+.addAnswer(barilocheMsg, {delay: 1000, media: "https://raw.githubusercontent.com/RojasCr/ChatBot-Turismo/refs/heads/main/images/Bariloche/Bariloche.jpeg?token=GHSAT0AAAAAADE7JFRSDZQZGHYEELDBXNNO2B7RGSA"})
 .addAnswer(reservarMsg, {capture: true}, async (ctx, {gotoFlow, globalState, state}) => {
 
     //console.log("Bari");
